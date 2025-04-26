b52937e3b439401b94af51f19b1411d6610edd9b
Dita574 <anggraedita574@gmail.com>
2025-04-26 10:24:16 +0700

    Update app (1).py

--- app (1).py
+++ app (1).py
@@ -5,6 +5,7 @@ st.set_page_config(page_title="Kalkulator pH & pOH", layout="centered")
 
 # Title
 st.title("🧪 Kalkulator pH & pOH Larutan kimia")
+st.header("_kalkulator_ is :blue[cool] :sunglasses:")
 
 # Description
 st.write("""
