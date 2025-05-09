e087069fd917b9bc019306a4695642baf9199a38
Dita574 <anggraedita574@gmail.com>
2025-05-09 17:01:16 +0700

    Update app (1).py

--- app (1).py
+++ app (1).py
@@ -9,26 +9,15 @@ st.header(":blue[Metode 1]")
 
 # Description
 st.write("""
-Aplikasi ini membantu menghitung nilai pH dan pOH larutan berdasarkan input konsentrasi ion [H⁺] atau [OH⁻].
+Kalkulator ini membantu menghitung titik sampling yang diperlukan pada cerobong dengan metode isokinetik
 """)
 
-# Sidebar for input
+'''# Sidebar for input
 with st.sidebar:
     st.header("Input Parameter")
     ion_type = st.radio("Pilih jenis ion yang diketahui:", ("[H⁺]", "[OH⁻]"))
     concentration = st.number_input(f"Masukkan konsentrasi {ion_type} (mol/L):", min_value=1e-14, max_value=1.0, format="%.2e")
-    show_dark_mode = st.checkbox("Aktifkan Mode Gelap")
-
-# Apply dark mode
-if show_dark_mode:
-    st.markdown(
-        """
-        <style>
-            body { background-color: #1e1e1e; color: white; }
-            .stApp { background-color: #1e1e1e; }
-        </style>
-        """, unsafe_allow_html=True
-    )
+    show_dark_mode = st.checkbox("Aktifkan Mode Gelap")'''
 
 # Divider
 st.markdown("---")
