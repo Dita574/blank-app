bd3f997725bbe8f2848ad9aca85414202c7cd25a
Dita574 <anggraedita574@gmail.com>
2025-05-12 02:05:54 +0700

    Update app (1).py

--- app (1).py
+++ app (1).py
@@ -46,7 +46,8 @@ if st.button("Hitung"):
         titik = 6
     elif D < 1:
         titik = 12
-    else: titik = 24
+    else: 
+        titik = 24 #Kondisi terburuk
 
 st.write(f"• Diameter cerobong: {D} m → Gunakan {titik} titik sampling")
     st.write(f"• Jumlah lubang sampling: {n_lubang}")
